d7ae0e8acbcd1b66cfd3864745a95cc3db2c9729
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2016-10-18 19:15:19 +0200

    Working logic structure

--- src/msg_processing_core.php
+++ src/msg_processing_core.php
@@ -10,6 +10,78 @@
 require_once('model/context.php');
 require_once('lib.php');
 
+function process_text_message($context, $text) {
+    $last_open_riddle_id = get_last_open_riddle_id();
+
+    $command = extract_command($text);
+    echo "Input text: {$text} (command: {$command})" . PHP_EOL;
+
+    if($command === 'new' && $context->is_abmin()) {
+        if($last_open_riddle_id === null) {
+            // New question!
+            $new_riddle_id = open_riddle();
+
+            Logger::info("New riddle ID: {$new_riddle_id}", __FILE__, $context);
+
+            $context->reply("Ok.");
+        }
+        else {
+            $context->reply("Riddle still open! Give answer");
+        }
+    }
+    else if($command === 'help') {
+        $context->reply("Ah, a <i>te</i> serve aiuto?");
+    }
+    else if($command === null) {
+        if($last_open_riddle_id === null) {
+            $context->reply("Nessun quiz attivo.");
+            if($context->is_abmin()) {
+                $context->reply("Devi attivare un quiz col comando /new.");
+            }
+            return;
+        }
+
+        if($context->is_abmin()) {
+            // Correct answer given
+            if(close_riddle($last_open_riddle_id, $text) === true) {
+                $context->reply("Riddle closed.");
+
+                // TODO: Update EVERYTHING
+
+                return;
+            }
+            else {
+                $context->reply("Failure");
+                return;
+            }
+        }
+        else {
+            // User answer given
+            $result = insert_answer($in->from_id, $text, $last_open_riddle_id);
+            $context->reply("Result: " . print_r($result, true));
+            return;
+        }
+    }
+    else {
+        $context->reply("Comando non previsto");
+    }
+}
+
+function process_message($context) {
+    if($context->get_message()->is_group()) {
+        // Group messaging not supported
+        error_log('Group message ignored');
+    }
+    else if($context->get_message()->is_private()) {
+        if($context->get_message()->is_text()) {
+            process_text_message($context, $context->get_message()->text);
+        }
+        else {
+            $context->reply("Messaggio non supportato");
+        }
+    }
+}
+
 // Set default timezone for date operations
 date_default_timezone_set('UTC');
 
@@ -17,53 +89,6 @@ date_default_timezone_set('UTC');
 $in = new IncomingMessage($message);
 $context = new Context($in);
 
-if($in->is_group()) {
-    // Group messaging not supported
-    error_log('Group message ignored');
-    die();
-}
-else if($in->is_private()) {
-    if($in->is_text()) {
-        $command = extract_command($in->text);
-        echo "Input text: {$in->text} (command: {$command})" . PHP_EOL;
+process_message($context);
 
-        if($command === 'new' && $context->is_abmin()) {
-            // New question!
-
-            $context->reply("Ok.");
-            return;
-        }
-
-        if($command === 'help') {
-            $context->reply("Ah, a <i>te</i> serve aiuto?");
-            return;
-        }
-
-        if($command === null) {
-            $riddle_id = get_last_open_riddle_id();
-            if($riddle_id === null) {
-                $context->reply("Nessun quiz attivo.");
-                if($context->is_abmin()) {
-                    $context->reply("Devi attivare un quiz col comando /new.");
-                }
-                return;
-            }
-
-            if($context->is_abmin()) {
-                // Correct answer given
-            }
-            else {
-                // User answer given
-                $result = insert_answer($in->from_id, $in->text, $riddle_id);
-                $context->reply("Result: " . print_r($result, true));
-                return;
-            }
-        }
-
-
-        $context->reply("Come, scusa?");
-    }
-    else {
-        $context->reply("Messaggio non supportato");
-    }
-}
+// Done! Bye bye
