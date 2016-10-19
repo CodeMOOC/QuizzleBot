38828d3a57359a40aa54ad8913b74720601e1913
Saverio Delpriori <saverio.delpriori@uniurb.it>
2016-10-19 12:06:06 +0200

    Update DB structure

--- sql/quizzlebot.sql
+++ sql/quizzlebot.sql
@@ -3,12 +3,18 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 18, 2016 at 04:56 PM
+-- Generation Time: Oct 19, 2016 at 11:59 AM
 -- Server version: 5.5.46-0+deb8u1
 -- PHP Version: 5.6.17-0+deb8u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `quizzlebot`
@@ -30,6 +36,22 @@ CREATE TABLE `answer` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `indentity`
+--
+
+CREATE TABLE `indentity` (
+  `telegram_id` int(11) UNSIGNED NOT NULL,
+  `first_name` varchar(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `full_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `group_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `participants_count` int(11) NOT NULL DEFAULT '1',
+  `status` int(11) NOT NULL DEFAULT '0',
+  `riddle_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `riddle`
 --
 
@@ -37,7 +59,9 @@ CREATE TABLE `riddle` (
   `id` int(11) UNSIGNED NOT NULL,
   `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `end_time` timestamp NULL DEFAULT NULL,
-  `answer` tinytext CHARACTER SET utf8 COLLATE utf8_unicode_ci
+  `answer` tinytext CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+  `salt` varchar(5) COLLATE utf8_bin NOT NULL,
+  `qrcode_path` varchar(4096) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -50,6 +74,12 @@ CREATE TABLE `riddle` (
 ALTER TABLE `answer`
   ADD PRIMARY KEY (`telegram_id`,`riddle_id`) USING BTREE,
   ADD KEY `riddle_id_index` (`riddle_id`) USING BTREE;
+
+--
+-- Indexes for table `indentity`
+--
+ALTER TABLE `indentity`
+  ADD PRIMARY KEY (`telegram_id`);
 
 --
 -- Indexes for table `riddle`
@@ -65,7 +95,7 @@ ALTER TABLE `riddle`
 -- AUTO_INCREMENT for table `riddle`
 --
 ALTER TABLE `riddle`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- Constraints for dumped tables
 --
@@ -75,3 +105,7 @@ ALTER TABLE `riddle`
 --
 ALTER TABLE `answer`
   ADD CONSTRAINT `riddle_id_fkey` FOREIGN KEY (`riddle_id`) REFERENCES `riddle` (`id`) ON DELETE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
